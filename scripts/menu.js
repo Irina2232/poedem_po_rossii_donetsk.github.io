c76d03c5572d401cee01a237ed2b6899e3ca7c0c
drbenzo <ekanwey@gmail.com>
2025-04-02 16:20:13 +0300

    адаптация под мобильные устройства и добавление меню

---
+++ scripts/menu.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const menuToggle = document.querySelector('.menu-toggle');
+    const menuList = document.querySelector('.menu-list');
+
+    menuToggle.addEventListener('click', () => {
+        menuList.classList.toggle('active');
+    });
+});
